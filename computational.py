3e5156ef04a907676022467e6c16b5e2c46091a0
Saadiya Abdulqader <139444895+Saadiya222@users.noreply.github.com>
2024-03-30 00:28:35 +0300

    Update computational.py

--- computational.py
+++ computational.py
@@ -3,10 +3,10 @@ from implementation import A_star_map_coloring
 from testCases import scenarios
 
 for name, graph in scenarios.items():
-    start_time = time.time()  # Capture start time
-    solution = A_star_map_coloring(graph)  # Run the algorithm
-    end_time = time.time()  # Capture end time
-    # Calculate elapsed time in milliseconds
+    start_time = time.time()  
+    solution = A_star_map_coloring(graph) 
+    end_time = time.time()  
+   
     elapsed_time_ms = (end_time - start_time) * 1000
     print(f"{name}: {'Solution found with colors in' if solution else 'No solution'} {elapsed_time_ms} ms \n")
     
